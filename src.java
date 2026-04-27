94bb742315bdf669efa1c7e3c676dca2ae5f2aec
B.Neeraj <88584739+sparkyjolteon@users.noreply.github.com>
2026-04-28 04:39:55 +0530

    Update src.java

--- src.java
+++ src.java
@@ -1,1 +1,74 @@
+public class QuantityMeasurementApp {
 
+    // -------------------- FEET CLASS --------------------
+    static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj) return true;
+
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+
+            Feet other = (Feet) obj;
+
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    // -------------------- INCHES CLASS --------------------
+    static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj) return true;
+
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+
+            Inches other = (Inches) obj;
+
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    // -------------------- STATIC METHODS --------------------
+
+    // Feet equality check
+    public static boolean checkFeetEquality(double v1, double v2) {
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+        return f1.equals(f2);
+    }
+
+    // Inches equality check
+    public static boolean checkInchesEquality(double v1, double v2) {
+        Inches i1 = new Inches(v1);
+        Inches i2 = new Inches(v2);
+        return i1.equals(i2);
+    }
+
+    // -------------------- MAIN METHOD --------------------
+    public static void main(String[] args) {
+
+        // Feet comparison
+        boolean feetResult = checkFeetEquality(1.0, 1.0);
+        System.out.println("Feet Equal? " + feetResult);
+
+        // Inches comparison
+        boolean inchResult = checkInchesEquality(1.0, 1.0);
+        System.out.println("Inches Equal? " + inchResult);
+    }
+}
